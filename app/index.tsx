da7ab0501654f213b84fe37479f732029992a89d
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-06-17 13:01:53 -0400

    - add package for uuid  - add function for deleting item and updating status

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,127 @@
 import {
+  FlatList,
   StyleSheet,
+  Switch,
   Text,
   TextInput,
   TouchableOpacity,
-  View
+  View,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+
+import {
+  useState
+} from "react";
+
+import { Ionicons } from '@expo/vector-icons';
+import uuid from 'react-native-uuid';
 import Header from '../components/Header';
 
-interface Todo {
+interface TaskData {
   id: string,
-  desc: string
+  desc: string,
+  status: boolean,
 }
 
+export default function App() {
 
-export default function Index() {
+  const [todoList, setTodoList] = useState<TaskData[]>(
+    [
+      { id: String(uuid.v4()), desc: "Do quiz1", status: true },
+      { id: String(uuid.v4()), desc: "Do quiz2", status: true },
+      { id: String(uuid.v4()), desc: "Do lab1", status: false },
+      { id: String(uuid.v4()), desc: "Do lab2", status: false },
+    ]
+  )
+
+  const renderTask = ({ item }: any) => {
+
+    const deleteItem = async () => {
+      console.log("* delete item: " + item.id)
+
+      const newTodoList: TaskData[] = [];
+
+      todoList.filter(t => t.id != item.id).map(
+        (t) => {
+          console.log(t)
+          newTodoList.push(t)
+        }
+      )
+      setTodoList(newTodoList)
+    }
+
+    const changeStatus = async () => {
+      console.log("* change status: " + item.id)
+
+      const newTodoList: TaskData[] = [];
+
+      todoList.map(
+        (t) => {
+          if (t.id == item.id) {
+            t.status = !t.status
+          }
+          newTodoList.push(t)
+        }
+      )
+      setTodoList(newTodoList)
+    }
+
+    return (
+      <>
+        <Text></Text>
+        <Text></Text>
+        <Text>{item.id} </Text>
+        <Text>{item.desc}</Text>
+        <Text>----------------------------</Text>
+        <TouchableOpacity onPress={deleteItem}>
+          <Ionicons name='trash-bin-outline' size={24} color="red" />
+        </TouchableOpacity>
+        <Switch
+          value={item.status}
+          onValueChange={changeStatus}
+        />
+      </>
+    )
+  }
+
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
       <Header title="Todo Input" />
-      <View style={styles.center} >
+      <View style={{}} >
         <TextInput placeholder="Enter Todo" />
         <TouchableOpacity>
           <Text> Add Item</Text>
         </TouchableOpacity>
       </View >
-      <Header title="Todo List" />
 
-    </SafeAreaView >
+      <Header title="Todo List" />
+      <View>
+        <FlatList
+          data={todoList}
+          renderItem={renderTask}
+          keyExtractor={(todoList) => todoList.id} />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
-  }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
 });
